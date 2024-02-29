e3f4e7587400c3ac6a1145bcccc97858dd1508fb
Mohamed Amine Amir <mamir@e1r11p10.1337.ma>
2024-02-29 12:17:20 +0100

    pollock

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include <mlx.h>
 #include <stdlib.h>
+# define WIDTH 1000
+# define HEIGHT 500
 
 int main()
 {
-    void *mlx_ptr;
+    void    *mlx_cnx;
+    void    *mlx_win;
+    int i,j;
 
-    mlx_ptr = mlx_init();
-    mlx_destroy_display(mlx_ptr);
-    free(mlx_ptr);
+    i = HEIGHT * 0.1;
+    mlx_cnx = mlx_init();
+    mlx_win = mlx_new_window(mlx_cnx, WIDTH, HEIGHT , "So_long");
+    while (i < HEIGHT * 0.9)
+    {
+        j = WIDTH * 0.1;
+       while(j < WIDTH * 0.9)
+       {
+            mlx_pixel_put(mlx_cnx, mlx_win, j, i, rand() % 0xF0FF000);
+            ++j;
+       }
+        ++i;
+    }
+    mlx_loop(mlx_cnx);
+    free(mlx_cnx);
 }
